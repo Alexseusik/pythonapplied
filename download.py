2ba59b34162af7b2c969d38f1f076848ab5c5551
alexsey <alexgnibeda03@gmail.com>
2022-02-01 23:21:46 +0200

    adding 26.03b

--- download.py
+++ download.py
@@ -3,7 +3,7 @@ import re
 import os
 
 
-TOPIC = r'<a href="(?P<URL>.*?)" >Тема {}\..*?</a>'
+TOPIC = r'<a href=\"(?P<URL>.*?)\" >Тема {}\..*?</a>'
 PYFILE = r'"(.+?\.pyw*)"'
 NAME = r'.*/(?P<NAME>.+)'
 
@@ -18,11 +18,13 @@ enc = info.get_content_charset()
 html = str(response.read(), encoding=enc)
 
 match = re.search(TOPIC.format(theme), html)
+print(match)
 
 if match:
     topic = match.group("URL")
     response = urlopen(url + topic)
     html_topic = str(response.read(), encoding=enc)
+    print(topic)
     if not os.path.exists("pyfiles"):
         os.mkdir("pyfiles")
     for pyfile in re.findall(PYFILE, html_topic):
@@ -31,4 +33,3 @@ if match:
         urlretrieve(url + pyfile,
                     os.path.join('pyfiles', name))
         print(name)
-
