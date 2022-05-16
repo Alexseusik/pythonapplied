6e00264a8edebd87e2cc4b3a5b892f193d6dc854
Oleksii Hnybida <alexgnibeda03@gmail.com>
2022-05-16 18:38:25 +0300

    adding 29.13 to main branch

--- t28.03a/trees.py
+++ t28.03a/trees.py
@@ -13,6 +13,7 @@ db = 'database.db'
 conn = sqlite3.connect(db)
 cur = conn.cursor()
 
+
 # print([item[0] for item in cur.execute('select * from type')])
 
 
@@ -21,7 +22,7 @@ def adding_type_to_db():
     if user_input_type in [item[0] for item in cur.execute('select * from type')]:
         print('Такий рід дерев вже існує у базі!')
     else:
-        cur.execute('insert into type(type) values (?)', (user_input_type, ))
+        cur.execute('insert into type(type) values (?)', (user_input_type,))
         conn.commit()
         conn.close()
 
@@ -29,7 +30,7 @@ def adding_type_to_db():
 def adding_sort_to_db():
     user_input_type, user_input_sort = input('Введіть рід та сорт дерева через пробіл, який треба додати:  ').split()
 
-    check_type = bool([el[0] for el in cur.execute('''select * from type where type = ? ''', (user_input_type, ))])
+    check_type = bool([el[0] for el in cur.execute('''select * from type where type = ? ''', (user_input_type,))])
     # True if type exist in type, False if not exist ^
 
     check_sort = bool([el[0] for el in cur.execute('''select * from sort where sort = ? and type = ? ''',
@@ -53,7 +54,7 @@ def adding_sort_to_db():
 
 
 def adding_tree_to_db():
-    user_input_tree_id, user_input_tree_type, user_input_tree_sort, user_input_tree_year, user_input_tree_place\
+    user_input_tree_id, user_input_tree_type, user_input_tree_sort, user_input_tree_year, user_input_tree_place \
         = input('Введіть id, рід, сорт, рік посадки, місце посадки дерева через пробіл, який треба додати:  ').split()
     check_type = bool([el[0] for el in cur.execute('''select * from type where type = ? ''', (user_input_tree_type,))])
     check_sort = bool([el[0] for el in cur.execute('''select * from sort where sort = ? and type = ? ''',
@@ -80,7 +81,7 @@ def adding_harvest_to_db():
     for row in cur.execute('''select * from tree_info'''):
         print(row)
     print('\nУгорі дерева які вже є у базі даних\n')
-    user_input_harvest_id, user_input_tree_id, user_input_year, user_input_amount\
+    user_input_harvest_id, user_input_tree_id, user_input_year, user_input_amount \
         = input('Введіть id врожаю, id дерева, рік врожаю, кількість врожаю який треба додати:  ').split()
     check_tree = bool([el[0] for el in cur.execute('''select * from tree_info where id == ? ''',
                                                    (user_input_tree_id,))])
@@ -102,7 +103,63 @@ def adding_harvest_to_db():
 
 
 def all_trees_of_same_type():
-    pass
+    for row in cur.execute('''select * from tree_info'''):
+        print(row)
+    result = None
+    user_input_tree_type = input('Введіть тип дерева які хочете знайти у таблиці усіх дерев:  ')
+    if user_input_tree_type in [el[1] for el in cur.execute('''select * from tree_info''')]:
+        result = cur.execute('''select * from tree_info where type = ?''', (user_input_tree_type,))
+    if result is not None:
+        print('\n!!!Here all of yours result!!!\n')
+        for row in result:
+            print(row)
+    else:
+        print('\nSuch a type is not in tree_info table :(\n')
 
 
-adding_harvest_to_db()
+def select_harvest_of_year_range():
+    for el in cur.execute('''select * from harvest_info'''):
+        print(el)
+    user_input_tree_id, user_input_start_year, user_input_end_year = \
+        input('Введіть через пробіл id дерева, початок і '
+              'кінець періоду для якого треба вивести увесь врожай  ').split()
+    user_input_tree_id = int(user_input_tree_id)
+    result = None
+    if int(user_input_tree_id) in [el[1] for el in cur.execute('''select * from harvest_info''')]:
+        result = cur.execute('''select * from harvest_info where (tree_id = ?) and (year between ? and ?)''',
+                             (user_input_tree_id, user_input_start_year, user_input_end_year))
+    if result is not None:
+        print(f'Here is all harvest from tree with id {user_input_tree_id}')
+        for row in result:
+            print(row)
+    else:
+        print('Here is not such a tree :(')
+
+
+i = 0
+while i != 7:
+    user_input = int(input(
+        'Введіть фунцкію яку треба виконати:\n'
+        '1 - Додати новий тип дерева\n'
+        '2 - Додати новий сорт дерева\n'
+        '3 - Додати нове дерево до бази\n'
+        '4 - Додати врожай до дерева\n'
+        '5 - Обрати усі дерева потрібного типу\n'
+        '6 - Обрати врожай якогось дерева за потрібний проміжок\n'
+        '7 - Зупинити программу\n'))
+
+    match user_input:
+        case 1:
+            adding_type_to_db()
+        case 2:
+            adding_sort_to_db()
+        case 3:
+            adding_tree_to_db()
+        case 4:
+            adding_harvest_to_db()
+        case 5:
+            all_trees_of_same_type()
+        case 6:
+            select_harvest_of_year_range()
+        case 7:
+            exit()
